66fc7a32f65cd317122c7efbe893dcdbae22b896
MarouaneBouf <104838272+MarouaneBouf@users.noreply.github.com>
2022-08-30 00:28:15 +0100

    Create vowel_count.c

---
+++ 7kyu/Vowel Count/vowel_count.c
@@ -0,0 +1,54 @@
+/*
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+
+*/
+
+
+
+
+// Method 01:
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+size_t get_count(const char *s)
+{
+  int count = 0;
+  for(int i = 0 ; i < strlen(s); i++)
+  {
+    if(s[i] == 'a' || s[i] == 'e' || s[i] == 'u' || s[i] == 'o' || s[i] == 'i')
+    {
+      count ++;
+    }
+  }
+  return count;
+}
+
+// Method 02:
+
+/* WE USE strchr() ; The C library function char *strchr(const char *str, int c) searches 
+for the first occurrence of the character c (an unsigned char) in the string pointed to by the argument str. */
+
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+
+#define vowels "aeoui"
+size_t get_count(const char *s)
+{
+  int count = 0;
+  for(;*s != '\0';s++)
+  {
+    if(strchr(vowels,*s) != NULL)
+    {
+      count++;
+    }
+  }
+  return count;
+}
