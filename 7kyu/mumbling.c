cb0e8709b1e9c0977c43e093a119b595cf3a79e4
MarouaneBouf <104838272+MarouaneBouf@users.noreply.github.com>
2022-08-30 23:16:37 +0100

    Create mumbling.c
    https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/solutions/c

---
+++ 7kyu/mumbling.c
@@ -0,0 +1,48 @@
+/*
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+char *accum(const char *source);
+
+int main(void)
+{
+    char *source = "rqgsxa";
+    printf("%s\n",accum(source));
+    return 0;
+}
+
+char *accum(const char *source)
+{
+    char *res = malloc(50*sizeof(char));
+    int ind = 0;
+    int k;
+    for (int i = 0; source[i] != '\0'; i++)
+    {
+        k = 0;
+        res[ind] = toupper(source[i]);
+        ind ++;
+        while(k != i)
+        {
+            res[ind] = tolower(source[i]);
+            ind++;
+            k++;
+        }
+        if (source[i+1] != '\0')
+        {
+            res[ind] = '-';
+        }
+        ind++;
+        k = 0;
+    }
+    res[ind] = '\0';
+    return res;
+    free(res);
+}
