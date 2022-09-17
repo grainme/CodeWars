18652d26cc317012a2816e3154d746d87dab75c5
MarouaneBouf <marouanebouf3@gmail.com>
2022-09-17 11:40:13 +0100

    Reverse Vowels kata

---
+++ 7kyu/reverse_Vowels/reverse_vowels.py
@@ -0,0 +1,13 @@
+s = input()
+d = ""
+k = 0
+v = "aeoiuAEUOI"
+x = "".join(i for i in s if i in v)
+x = x[::-1]
+for i in range(len(s)):
+    if s[i] in v:
+        d+=x[k]
+        k+=1
+    else:
+        d+=s[i]
+print(d)
